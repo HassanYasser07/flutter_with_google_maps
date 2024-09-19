9622b0e253495258e79a1d51491be738a4dbe19a
hassanyasser <hassanyasser1313@gmail.com>
2024-09-20 01:05:01 +0300

    polylines

--- lib/custom_google_map/custom_google_map.dart
+++ lib/custom_google_map/custom_google_map.dart
@@ -1,5 +1,7 @@
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:flutter/material.dart';
+
+import '../models/place_model.dart';
 class CustomGoogleMap extends StatefulWidget {
   const CustomGoogleMap({super.key});
 
@@ -15,48 +17,68 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
   void initState() {
     super.initState();
     initMarkers();
+    initPolyline();
     initialCameraPostion = const CameraPosition(
-      zoom: 16,
+        zoom: 16,
         target: LatLng(31.236096292977177, 29.948132578022626));
-
   }
+
   @override
   void dispose() {
     googleMapController.dispose();
     super.dispose();
   }
-     Set<Marker> markers={};
+
+  Set<Marker> markers = {};
+  Set<Polyline> polylines = {};
+
   Widget build(BuildContext context) {
     return GoogleMap(
+      polylines: polylines,
       markers: markers,
-      onMapCreated: (controller){
+      onMapCreated: (controller) {
         googleMapController = controller;
         initMapStyle();
-
       },
-      initialCameraPosition:initialCameraPostion,
+      initialCameraPosition: initialCameraPostion,
       cameraTargetBounds: CameraTargetBounds(LatLngBounds(
           southwest: const LatLng(31.080569617326795, 29.763491041232577),
           northeast: const LatLng(31.30846738149212, 30.169298507189573))),
 
 
-
     );
   }
+
   void initMapStyle() async {
     var nightMapStyle = await DefaultAssetBundle.of(context)
         .loadString('assets/sunney_map_style.json');
     googleMapController.setMapStyle(nightMapStyle);
   }
+
   void initMarkers() {
-    var myMarker = const Marker(
-        markerId: MarkerId('1'),
-        position: LatLng(31.187084851056554, 29.928110526889437));
-    markers.add(myMarker);
+    var myPlaces = places
+        .map((placeMarker) =>
+        Marker(
+          infoWindow: InfoWindow(title: placeMarker.name),
+          position: placeMarker.latLng,
+          markerId: MarkerId(placeMarker.id.toString()),
+        ))
+        .toSet();
+
+    markers.addAll(myPlaces);
   }
+
+  void initPolyline() {
+    Polyline polyline = const Polyline(polylineId: PolylineId('1'), points: [
+      LatLng(31.226208780435154, 29.960687255469185),
+      LatLng(31.225364720875195, 29.954593276341214),
+      LatLng(31.231750027730584, 29.955365752568703),
+      LatLng(31.236593765802407, 29.95060214916585),
+    ]);
+    polylines.add(polyline);
+  }
+
 }
-
-
 
 
 
