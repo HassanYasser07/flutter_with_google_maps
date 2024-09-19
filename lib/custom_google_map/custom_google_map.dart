1e1b0e65eb3e714a5c1ec5f8d44e55124978778c
hassanyasser <hassanyasser1313@gmail.com>
2024-09-20 00:52:03 +0300

    add marker

--- lib/custom_google_map/custom_google_map.dart
+++ lib/custom_google_map/custom_google_map.dart
@@ -10,16 +10,30 @@ class CustomGoogleMap extends StatefulWidget {
 class _CustomGoogleMapState extends State<CustomGoogleMap> {
   @override
   late CameraPosition initialCameraPostion;
+  late GoogleMapController googleMapController;
 
   void initState() {
     super.initState();
+    initMarkers();
     initialCameraPostion = const CameraPosition(
       zoom: 16,
         target: LatLng(31.236096292977177, 29.948132578022626));
 
   }
+  @override
+  void dispose() {
+    googleMapController.dispose();
+    super.dispose();
+  }
+     Set<Marker> markers={};
   Widget build(BuildContext context) {
     return GoogleMap(
+      markers: markers,
+      onMapCreated: (controller){
+        googleMapController = controller;
+        initMapStyle();
+
+      },
       initialCameraPosition:initialCameraPostion,
       cameraTargetBounds: CameraTargetBounds(LatLngBounds(
           southwest: const LatLng(31.080569617326795, 29.763491041232577),
@@ -28,6 +42,17 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
 
 
     );
+  }
+  void initMapStyle() async {
+    var nightMapStyle = await DefaultAssetBundle.of(context)
+        .loadString('assets/sunney_map_style.json');
+    googleMapController.setMapStyle(nightMapStyle);
+  }
+  void initMarkers() {
+    var myMarker = const Marker(
+        markerId: MarkerId('1'),
+        position: LatLng(31.187084851056554, 29.928110526889437));
+    markers.add(myMarker);
   }
 }
 
